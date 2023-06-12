746adf834a0df494c268bf6e93884efe9c65ca1d
Batuhan Güçlü <batuhan.gueclue@gmail.com>
2023-06-12 19:20:15 +0200

    Mobile

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import "../style/globals.css";
-import "../style/navbar.css";
-import "../style/home.css";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
