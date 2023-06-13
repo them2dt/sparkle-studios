37f59f22545abd6f59d38c533258481d59097409
Batuhan Güçlü <batuhan.gueclue@gmail.com>
2023-06-13 17:02:55 +0200

    back to old

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import "../style/globals.css";
+import "../style/navbar.css";
+import "../style/home.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+
+
