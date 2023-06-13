84064b86b0857790e85000c2a1d97fadeb189b80
Batuhan Güçlü <batuhan.gueclue@gmail.com>
2023-06-14 01:06:33 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function App() {
   return (
     <div className="app" ref={ref}>
       <Head>
-        <title><img src="sparklestudios.png" alt="Sparkle Studios"></img></title>
+        <title>Spa</title>
       </Head>
       <div id="navigation" className="navigation">
         <div className="navbar">
@@ -43,7 +43,7 @@ export default function App() {
       <div className="intro">
         <div className="intro-content">
           <div className="intro-text">
-            <div className="intro-title">Sparkle Studios</div>
+            <div className="intro-title"><img src="sparklestudios.png" alt="Sparkle Studios"></img></div>
             <div className="intro-line"></div>
             <div className="intro-description">
               Boost your business with us.
