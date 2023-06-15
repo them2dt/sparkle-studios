b66d0ebfaa17f0d029e8210e1fc1ac6d2ffa32d5
Batuhan Güçlü <batuhan.gueclue@gmail.com>
2023-06-15 15:14:52 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function App() {
       <div className="intro">
         <div className="intro-content">
           <div className="intro-text">
-            <div className="intro-title"><img src="A.png" alt="SparkleStudios"/></div> 
+            <img className="intro-title" src="A.png" alt="SparkleStudios"/>
             <div className="intro-line"></div>
             <div className="intro-description">
               Boost your business with us.
